6c8f7ce1867be88617a76f216b2eb1cbdc84f75b
weiya <wave.weiya@gmail.com>
2022-02-22 19:06:52 +0800

    feature: update start scripts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = 3000;
+  const port = 6668;
 
   console.log('==================== 🔥🔥🔥🔥🔥🔥🔥🔥🔥 ===================');
 
