ee316e03a49081b23e4e8d9cfce81a88d026fa75
weiya <wave.weiya@gmail.com>
2022-02-23 19:15:00 +0800

    feature: update crud

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { Logger } from './logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   const port = 8000;
 
