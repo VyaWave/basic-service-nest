a2c4959c8ea5b3cda96bd08aa039432a539126fb
weiya <wave.weiya@gmail.com>
2022-03-01 16:29:49 +0800

    fix: host

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,6 @@ import devCfg from './dev.config';
 import prodCfg from './prod.config';
 const isOnline = process.env.NODE_ENV === 'production';
 
-const appCfg = !isOnline ? prodCfg : devCfg;
+const appCfg = isOnline ? prodCfg : devCfg;
 
 export default appCfg;
