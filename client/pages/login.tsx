8ad71b6a25b948ca419d48f8a69fc5313dfc6eab
weiya <wave.weiya@gmail.com>
2022-03-24 11:21:27 +0800

    feature: add account login page

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,16 +1,34 @@
+import React from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Login.module.css';
 
-import { Panel, InputGroup, Input, Button } from 'rsuite';
+import { InputGroup, Input, Button } from 'rsuite';
+import SortUpIcon from '@rsuite/icons/SortUp';
+import EmailIcon from '@rsuite/icons/Email';
+import EyeCloseIcon from '@rsuite/icons/EyeClose';
+import VisibleIcon from '@rsuite/icons/Visible';
+import CharacterLockIcon from '@rsuite/icons/CharacterLock';
+
+import styles from '../styles/Login.module.css';
 
 const layout = {
   width: 300,
-  marginBottom: 10,
+  marginBottom: 20,
 };
 
 const Login: NextPage = () => {
+  const [visible, setVisible] = React.useState(false);
+  const [isLogin, setLogin] = React.useState(true);
+
+  const handleInputChangeChange = () => {
+    setVisible(!visible);
+  };
+
+  const handleLoginChangeChange = () => {
+    setLogin(!isLogin);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,19 +36,47 @@ const Login: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <Panel header="Panel title" bordered className={styles.panelItem}>
-          <InputGroup inside style={layout}>
-            <InputGroup.Addon>1</InputGroup.Addon>
-            <Input />
+        <div className={styles.ctxWrapper}>
+          <InputGroup inside style={layout} className={styles.input}>
+            <InputGroup.Addon>
+              <EmailIcon />
+            </InputGroup.Addon>
+            <Input placeholder="Your Account" />
           </InputGroup>
 
-          <InputGroup inside style={layout}>
-            <InputGroup.Addon>2</InputGroup.Addon>
-            <Input />
+          <InputGroup inside style={layout} className={styles.input}>
+            <InputGroup.Addon>
+              <CharacterLockIcon />
+            </InputGroup.Addon>
+            <Input
+              placeholder="Your Password"
+              type={visible ? 'text' : 'password'}
+            />
+            <InputGroup.Button onClick={handleInputChangeChange}>
+              {visible ? <EyeCloseIcon /> : <VisibleIcon />}
+            </InputGroup.Button>
           </InputGroup>
 
-          <Button type="submit">登录系统</Button>
-        </Panel>
+          <Button
+            type="submit"
+            className={styles.button}
+            appearance="primary"
+            style={{ width: 300 }}
+            color="blue"
+          >
+            {isLogin ? '登录系统' : '注册账号'}
+            <SortUpIcon
+              style={{
+                transform: 'rotate(90deg)',
+                fontSize: '20px',
+              }}
+            />
+          </Button>
+
+          <div className={styles.tips} onClick={handleLoginChangeChange}>
+            {isLogin ? 'Go To SignUp' : 'Go To Login'}{' '}
+          </div>
+        </div>
       </main>
     </div>
   );
